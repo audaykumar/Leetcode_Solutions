a4489ffbd209ab49c0656b8ebc397cb05054158e
audaykumar <aniket.udaykumar@gmail.com>
2022-05-12 17:52:54 +0800

    Java solution for 1209

--- Algorithms/1209. Remove All Adjacent Duplicates in String II/java/1209_remove_adjacent_duplicates.java
+++ Algorithms/1209. Remove All Adjacent Duplicates in String II/java/1209_remove_adjacent_duplicates.java
@@ -1,7 +1,42 @@
+import java.util.Stack;
+
 class Solution {
+    
+    public class charStack {
+        public char c;
+        public int count;
+    }
     public String removeDuplicates(String s, int k) {
         
-        return s;
+        Stack<charStack> stack = new Stack<>();
+
+        
+        for (int i = 0; i < s.length(); i++) {
+            // System.out.println(s.charAt(i));
+            if (!stack.isEmpty() && stack.peek().c == s.charAt(i)){
+
+                charStack temp = stack.pop();
+                temp.count += 1;
+
+                if (temp.count != k) {
+                    stack.push(temp);
+                }
+            } else {
+            charStack temp = new charStack();
+            temp.c = s.charAt(i);
+            temp.count = 1;
+            stack.push(temp);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < stack.size(); i++) {
+            String temp =String.valueOf(stack.elementAt(i).c).repeat(stack.elementAt(i).count);  
+            sb.append(temp);
+        }
+     
+        return sb.toString();
+        
     }
 
     public static void main(String[] args) {
